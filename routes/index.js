511f8c53112831614ca7cebedbbf4b4ac4ce8ab9
PesoaGaston <pesoagaston@MacBook-Pro-de-PesoaGaston.local>
2021-06-06 14:15:18 -0300

    test connect

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,15 @@ const express = require('express')
 router = express.Router();
 
 router.get('/', (req, res) => {
-    db.connect(()=>{
-
-        console.log(db);
-
-    });
+    // db.connect(() => {
+    // });
     dbGaston = db.getInstance();
     dbGaston.collection("tickets")
-        .find({"cliente.numeroCliente":1})
+        .find({ "cliente.numeroCliente": 1 })
         .toArray(function (err, items) {
             res.send(items);
-            dbGaston.close();
         });;
+    //db.close(() => { });
 })
 
 
