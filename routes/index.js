c016a2cc7c34821f003f089b5d3dc581b9a38a7b
PesoaGaston <pesoagaston@MacBook-Pro-de-PesoaGaston.local>
2021-06-12 10:54:14 -0300

    desperfectos

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,24 @@ router.get('/', (req, res) => {
         .find({ "cliente.numeroCliente": 1 })
         .toArray(function (err, items) {
             res.send(items);
-        });;
+        });
     //db.close(() => { });
+})
+
+router.get('/desperfectos', (req, res) => {
+    dbGaston = db.getInstance();
+    dbGaston.collection("tickets")
+        .aggregate([
+            {
+                $group: {
+                    _id: { motivo: "$motivo" }
+                }
+            }
+        ]).toArray( (err, result) => {
+            if (err) return console.log(err)
+            console.log(result)
+           res.send(result)
+        })
 })
 
 
